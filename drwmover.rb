f00a366ccb4f893764a53be211927f66c8b821d9
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-03 20:18:57 +0900

    check if dir exists

--- drwmover.rb
+++ drwmover.rb
@@ -1,4 +1,16 @@
 require 'optparse'
+require 'logger'
+
+logger = Logger.new(STDOUT)
 
 params = ARGV.getopts("", "source:", "target:")
-p params
+source = params["source"]
+target = params["target"]
+
+unless Dir.exist?(source)
+  logger.error("Source dir not found.")
+end
+
+unless Dir.exist?(target)
+  logger.error("Target dir not found.")
+end
