58f68f170cd386693f13545c5e63fef22aeb5dda
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-03 20:31:17 +0900

    check if file exists

--- drwmover.rb
+++ drwmover.rb
@@ -3,9 +3,11 @@ require 'logger'
 
 logger = Logger.new(STDOUT)
 
-params = ARGV.getopts("", "source:", "target:")
+params = ARGV.getopts("", "source:", "target:", "name:")
+
 source = params["source"]
 target = params["target"]
+name = params["name"]
 
 unless Dir.exist?(source)
   logger.error("Source dir not found.")
@@ -14,3 +16,14 @@ end
 unless Dir.exist?(target)
   logger.error("Target dir not found.")
 end
+
+file_path = File.join(source, name)
+unless file_path.end_with?(".png")
+  file_path += ".png"
+end
+
+unless File.exist?(file_path)
+  logger.error("File not found.")
+end
+
+p file_path
