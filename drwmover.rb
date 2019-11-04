b158e9a355c82ebf8100efead64a773d2ebac488
Yoshihiro Tanaka <contact@cordea.jp>
2019-11-04 11:05:03 +0900

    simplify

--- drwmover.rb
+++ drwmover.rb
@@ -3,6 +3,7 @@ require 'logger'
 
 DPI = %w(mdpi hdpi xhdpi xxhdpi xxxhdpi)
 DRAWABLE_PATH = "src/main/res/drawable"
+SUFFIX = ".png"
 
 logger = Logger.new(STDOUT)
 
@@ -11,6 +12,7 @@ params = ARGV.getopts("", "source:", "target:", "name:")
 source = params["source"]
 target = params["target"]
 name = params["name"]
+file_name = name + SUFFIX
 
 unless Dir.exist?(source)
   logger.error("Source dir not found.")
@@ -20,12 +22,7 @@ unless Dir.exist?(target)
   logger.error("Target dir not found.")
 end
 
-file_path = File.join(source, name)
-unless file_path.end_with?("*.png")
-  file_path += "*.png"
-end
-
-files = Dir.glob(file_path)
+files = Dir.glob(File.join(source, name + "*.png"))
 if files.empty?
   logger.error("File not found.")
 end
